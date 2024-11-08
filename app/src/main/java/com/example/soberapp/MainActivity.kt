cc99aa21b3276a7fe2644b8a90ac277f5eb72105
adamgeo1 <adamgeo66@gmail.com>
2024-11-07 23:47:13 -0500

    added counter button and centered it. also removed default Greeting function.

--- app/src/main/java/com/example/soberapp/MainActivity.kt
+++ app/src/main/java/com/example/soberapp/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.soberapp.ui.theme.SoberAppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +28,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             SoberAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CounterButton()
+                    }
                 }
             }
         }
@@ -31,17 +43,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SoberAppTheme {
-        Greeting("Android")
+fun CounterButton(modifier: Modifier = Modifier) {
+    var num by remember { mutableIntStateOf(0) }
+    Button(
+        onClick = {
+            num++
+        },
+        modifier = modifier,
+    ) {
+        Text(num.toString())
     }
 }
