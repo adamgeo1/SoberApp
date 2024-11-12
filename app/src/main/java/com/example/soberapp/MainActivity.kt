e411239e29f6226897527311e1ed69a3a22735eb
adamgeo1 <adamgeo66@gmail.com>
2024-11-11 20:28:41 -0500

    changed PlusButton icon to plus.xml, added label to day counter

--- app/src/main/java/com/example/soberapp/MainActivity.kt
+++ app/src/main/java/com/example/soberapp/MainActivity.kt
@@ -7,26 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonElevation
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import com.example.soberapp.ui.theme.SoberAppTheme
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.soberapp.ui.theme.SoberAppTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -45,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         Column(
                             horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.spacedBy(space = Dp(16f)),
+                            verticalArrangement = Arrangement.spacedBy(space = 16.dp),
                             content = {
                                 Counter(num)
                                 PlusButton(num)
@@ -61,20 +61,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Counter(num: MutableState<Int>, modifier: Modifier = Modifier) {
     Text(
-        text = num.value.toString(),
-        modifier = modifier
+        text = "Time Sober:\n\n" + num.value.toString(),
+        textAlign = Center,
+        modifier = modifier,
+        fontSize = 56.sp
     )
 }
 
 @Composable
 fun PlusButton(num: MutableState<Int>, modifier: Modifier = Modifier) {
-    Button(
+    IconButton(
         onClick = {
             num.value++
         },
-        modifier = modifier,
-        shape = CircleShape,
+        modifier = modifier.size(96.dp)
     ) {
-        Text("+")
+        Icon(
+            painter = painterResource(R.drawable.plus),
+            contentDescription = "Plus Button",
+            tint = Color.Unspecified,
+            modifier = modifier.size(96.dp)
+        )
     }
 }
