5184e9551b3a6769c0b4728258b07213148c4b32
adamgeo1 <adamgeo66@gmail.com>
2024-11-11 19:47:12 -0500

    separated counter text and button

--- app/src/main/java/com/example/soberapp/MainActivity.kt
+++ app/src/main/java/com/example/soberapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -19,6 +25,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.Dp
 
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +34,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var num = remember { mutableIntStateOf(0) }
             SoberAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
@@ -34,7 +43,14 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding),
                         contentAlignment = Alignment.Center
                     ) {
-                        CounterButton()
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(space = Dp(16f)),
+                            content = {
+                                Counter(num)
+                                PlusButton(num)
+                            }
+                        )
                     }
                 }
             }
@@ -43,14 +59,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CounterButton(modifier: Modifier = Modifier) {
-    var num by remember { mutableIntStateOf(0) }
+fun Counter(num: MutableState<Int>, modifier: Modifier = Modifier) {
+    Text(
+        text = num.value.toString(),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun PlusButton(num: MutableState<Int>, modifier: Modifier = Modifier) {
     Button(
         onClick = {
-            num++
+            num.value++
         },
         modifier = modifier,
+        shape = CircleShape,
     ) {
-        Text(num.toString())
+        Text("+")
     }
 }
