8b905dd3003df8960c36f44aed0f18d7dc3e7004
adamgeo1 <adamgeo66@gmail.com>
2024-11-21 12:51:07 -0500

    refactored MainActivity.kt and added DataStore to build.gradle.kts to store days outside of app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.android
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -41,6 +43,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/soberapp/MainActivity.kt
+++ app/src/main/java/com/example/soberapp/MainActivity.kt
@@ -1,104 +1,131 @@
 package com.example.soberapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.example.soberapp.ui.theme.SoberAppTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.Period
-import kotlin.time.measureTime
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+val DAYS_KEY = intPreferencesKey("days")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var num = remember { mutableIntStateOf(0) }
+            var numState by remember { mutableStateOf(0)}
+            LaunchedEffect(Unit) {
+                numState = getDays(dataStore).first()
+            }
             SoberAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier.fillMaxSize()
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.spacedBy(space = 16.dp),
-                            content = {
-                                Counter(num)
-                                PlusButton(num)
-                            }
-                        )
-                    }
-                }
+                SoberAppScreen(
+                    num = numState,
+                    onIncrement = {
+                        numState++
+                        lifecycleScope.launch {
+                            saveDays(dataStore, numState)
+                        }
+                    })
+            }
+        }
+    }
+}
+
+suspend fun saveDays(dataStore: DataStore<Preferences>, days: Int) {
+    dataStore.edit { preferences ->
+        preferences[DAYS_KEY] = days
+    }
+}
+
+fun getDays(dataStore: DataStore<Preferences>): Flow<Int> {
+    return dataStore.data.map { preferences ->
+        preferences[DAYS_KEY] ?: 0
+    }
+}
+
+@Composable
+fun SoberAppScreen(num: Int, onIncrement: () -> Unit) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Counter(num)
+                PlusButton(onIncrement)
             }
         }
     }
 }
 
 @Composable
-fun Counter(num: MutableState<Int>, modifier: Modifier = Modifier) {
-    val yearsMonthsWeeksDays = daysToYearsMonthsWeeksDays(num.value)
+fun Counter(num: Int) {
+    val yearsMonthsWeeksDays = daysToYearsMonthsWeeksDays(num)
     Text(
         text = "Time Sober:\n",
-        textAlign = Center,
-        modifier = modifier,
+        textAlign = TextAlign.Center,
         fontSize = 56.sp
     )
     Text(
         text = yearsMonthsWeeksDays,
-        textAlign = Center,
-        modifier = modifier,
+        textAlign = TextAlign.Center,
         fontSize = 28.sp
     )
 }
 
 @Composable
-fun PlusButton(num: MutableState<Int>, modifier: Modifier = Modifier) {
+fun PlusButton(onIncrement: () -> Unit) {
     Box(
-        modifier = modifier
+        modifier = Modifier
             .size(96.dp)
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = rememberRipple(bounded = false, radius = 40.dp),
-                onClick = {
-                    num.value++
-                }
+                onClick = onIncrement
             )
-    ){
+    ) {
         Icon(
             painter = painterResource(R.drawable.plus),
             contentDescription = "Plus Button",
             tint = Color.Unspecified,
-            modifier = modifier.size(96.dp)
+            modifier = Modifier.size(96.dp)
         )
     }
 }
@@ -109,24 +136,14 @@ fun daysToYearsMonthsWeeksDays(days: Int): String {
     val period = Period.between(startDate, endDate)
     val years = period.years
     val months = period.months
-    val remainingDaysAfterMonths = period.days
-    val weeks = remainingDaysAfterMonths / 7
-    val daysLeft = remainingDaysAfterMonths % 7
-    if (years == 0) {
-        if (months == 0) {
-            if (weeks == 0) {
-                return "$days days"
-            }
-            else {
-                return "$weeks weeks, $daysLeft days"
-            }
-        }
-        else {
-            return "$months months, $weeks weeks, $daysLeft days"
-        }
-    }
-    else {
-        return "$years years, $months months, $weeks weeks, $daysLeft days"
-    }
+    val remainingDays = period.days
+    val weeks = remainingDays / 7
+    val daysLeft = remainingDays % 7
 
+    return buildString {
+        if (years > 0) append("$years years, ")
+        if (months > 0) append("$months months, ")
+        if (weeks > 0) append("$weeks weeks, ")
+        append("$daysLeft days")
+    }.trimEnd(',', ' ')
 }
